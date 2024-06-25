440fe6b63e6e362e271ac81f6b532c0e0443e952
NitroSniper <mahiemiah35@gmail.com>
2024-06-25 22:40:36 +0100

    refactored code

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,17 @@ use tower_http::services::ServeDir;
 
 #[derive(Template)]
 #[template(path = "hello.html")]
-struct HelloTemplate<'a> {
-    name: &'a str,
+struct HelloTemplate {
     dark: bool,
-}
-
-async fn templated_hello() -> HelloTemplate<'static> {
-    HelloTemplate {
-        name: "Nitro",
-        dark: true,
-    }
-}
-
-async fn hello_world() -> &'static str {
-    "Hello, wor"
 }
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-        .route(
-            "/",
-            get(|| async {
-                HelloTemplate {
-                    name: "Nitro",
-                    dark: false,
-                }
-            }),
-        )
+        .route("/", get(|| async { HelloTemplate { dark: false } }))
         .nest_service("/static", ServeDir::new("static"))
         .layer(tower_livereload::LiveReloadLayer::new());
-    // Needed for axum but not hyper
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, router).await.unwrap()
+    axum::serve(listener, router).await.unwrap();
 }
