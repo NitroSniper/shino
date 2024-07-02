6e6303194f2f501cf952099fc9610a9721083ffa
NitroSniper <mahiemiah35@gmail.com>
2024-07-02 17:12:14 +0100

    created a square effect

---
+++ flake.nix
@@ -0,0 +1,43 @@
+# in flake.nix
+{
+  inputs = {
+    # this is equivalent to `nixpkgs = { url = "..."; };`
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          buildInputs = [
+            (rust-bin.nightly.latest.default.override { extensions = [ "rust-analyzer" ]; })
+            tailwindcss
+            hey
+            watchexec
+            djlint
+          ];
+        };
+      }
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,26 @@ module.exports = {
     "./templates/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'go-up-square': {
+          '0%': { transform: 'translateY(5000%) rotate(0)' },
+          '100%': { transform: 'translateY(0%) rotate(720deg)' },
+        }
+      },
+      animation: {
+        'up-square': 'go-up-square 10s linear',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
+  safelist: [
+    'delay-250',
+    'delay-500',
+    'delay-750',
+  ]
 }
 
