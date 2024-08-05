21002ab5a5736284e6bace00560a7e1c87ddcf6f
NitroSniper <mahiemiah35@gmail.com>
2024-08-05 01:56:35 +0100

    writing astro project cause I don't want to deal with server rn

--- flake.nix
+++ flake.nix
@@ -3,16 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    # rust dev toolchain
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
@@ -20,55 +10,15 @@
     inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
-        name = "shino";
-
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ (import inputs.rust-overlay) ];
-        };
-
-        # this is how we can tell crane to use our toolchain!
-        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
-          p: p.rust-bin.nightly.latest.default.override { }
-        );
-
-        bin = craneLib.buildPackage {
-          src = pkgs.lib.cleanSource ./.;
-          strictDeps = false;
-          postInstall = ''
-            mv resources/ $out/
-          '';
-        };
-
-        docker = pkgs.dockerTools.buildImage {
-          inherit name;
-          tag = "latest";
-
-          copyToRoot = [ bin ];
-
-          config = {
-            Cmd = [ "/bin/${name}" ];
-            WorkingDir = "/";
-          };
-        };
+        pkgs = import inputs.nixpkgs { inherit system; };
       in
       {
-        packages = {
-          inherit bin docker;
-          default = bin;
-        };
-        devShells.default = craneLib.devShell {
+        devShells.default = pkgs.mkShell {
 
-          inputsFrom = [ bin ];
-
-          packages = with pkgs; [
-            rust-analyzer
-            bacon
-            tailwindcss
-            hey
-            watchexec
-            djlint
-            # dive 
+          buildInputs = with pkgs; [
+            nodejs
+            node2nix
+            yarn
           ];
         };
 
