866128f8a1ca4d7084bbc93cad6aebed09b5984f
NitroSniper <mahiemiah35@gmail.com>
2024-08-14 17:14:37 +0100

    cloudflare doesn't work so I'm doing my may

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,7 @@ import solidJs from "@astrojs/solid-js";
 
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [solidJs(), tailwind()],
-    output: "server",
-    adapter: cloudflare({
-        platformProxy: {
-            enabled: true,
-        },
-    }),
 });
