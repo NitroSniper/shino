6835cfa188ce07babcbac1d2dbcc7b508a46654b
NitroSniper <mahiemiah35@gmail.com>
2024-08-14 16:32:48 +0100

    forgot import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import solidJs from "@astrojs/solid-js";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [solidJs(), tailwind()],
